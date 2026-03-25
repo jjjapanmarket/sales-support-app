055bb302a18878aab4fa429ff766f54a1a63263d
jjjapanmarket <ttjump.jun.0404@gmail.com>
2026-03-25 22:38:17 +0900

    Initial commit

---
+++ src/main/java/com/example/salessupportapp/SalesSupportAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.salessupportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesSupportAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesSupportAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/salessupportapp/SalesSupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salessupportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesSupportAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
